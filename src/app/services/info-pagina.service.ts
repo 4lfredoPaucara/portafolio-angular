153725283b543a13804c9ff12eb900b6112b53fc
Alfredo Paucara <4lfredo.paucara@gmail.com>
2024-06-18 21:21:35 -0400

    Fin de la Seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,20 +9,33 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any[] = [];
   constructor( private http: HttpClient ) {
 
     //console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+    }
 
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-          
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp['email']);
+    private cargarInfo(){
+          // Leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+         
+        this.cargada = true;
+        this.info = resp;
           
         });
-
     }
+  private cargarEquipo(){
+       // Leer el archivo JSON desde firebase
+       this.http.get('https://angular-html-1c109-default-rtdb.firebaseio.com/equipo.json')
+       .subscribe( (resp:any) => {
+          
+        this.equipo = resp;
+        
+        // console.log(resp);
+         
+       });
+  }
 }
